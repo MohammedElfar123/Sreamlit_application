8e29d49435b472b7d83078e110555d23a02d5a30
Mohamed Mahmoud <123865756+MohammedElfar123@users.noreply.github.com>
2024-09-15 20:45:44 +0300

    Update Banking_Coustmer_Prediction.py

--- Banking_Coustmer_Prediction.py
+++ Banking_Coustmer_Prediction.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import pickle
 
 # Load model
-Coustomer_Churn_Pre = '/Users/mohammedmahmood/Desktop/Final Project /Coustomer_Churn_Prediction.sav'
+Coustomer_Churn_Pre = 'Coustomer_Churn_Prediction.sav'
 model = pickle.load(open(Coustomer_Churn_Pre, 'rb'))
 
 
